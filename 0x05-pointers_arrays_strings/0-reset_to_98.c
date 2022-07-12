1c128f7f3f4f459ffde814a4dfd74c071bc99700
Godwin Akpan <godwinakpan9@gmail.com>
2022-07-12 14:23:50 -0700

    winz

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that update the reference of a pointer
- * @n: pointer to n to be updated
- * Return: void
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
  */
+
 void reset_to_98(int *n)
 {
 		*n = 98;
 }
-Footer
-
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-int _putchar(char);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,5 +10,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-Footer
 
